79b10ab3b152406a7166800248d636a83c6bec07
joimo <ga.jaimealves@gmail.com>
2021-05-05 23:42:28 -0300

    produto controlller: get all e get by id

---
+++ WebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Context;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext __context;
+
+        public ProdutosController(AppDbContext contexto)
+        {
+            __context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> GetProdutos()
+        {
+            return __context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Produto> GetProdutoById(int id)
+        {
+            Produto prod = __context.Produtos.AsNoTracking().Where(produto => produto.ProdutoId == id).FirstOrDefault();
+
+            if (prod == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return prod;
+            }
+        }
+    }
+}
